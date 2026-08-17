1af60881b7bb65bcd2fc200751d48b5a193d3f2e
john2772737 <88893071+john2772737@users.noreply.github.com>
2026-08-17 20:11:06 +0800

    first commit

---
+++ src/main/java/dev/john/claimsprocessingsystem/ClaimsProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.john.claimsprocessingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsProcessingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimsProcessingSystemApplication.class, args);
+    }
+
+}
